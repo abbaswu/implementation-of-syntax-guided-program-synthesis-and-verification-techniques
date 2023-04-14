545b210f8c514c9d8d37effe23b784e3e2f85005
Jifeng Wu <jifengwu2k@gmail.com>
2023-04-13 17:06:17 -0700

    2023-04-13T17:04:09-07:00

--- bottom_up_tree_search.py
+++ bottom_up_tree_search.py
@@ -34,17 +34,12 @@ def leaf_expression_size(leaf_expression, terminals):
     )
 
 
-def bottom_up_tree_search(
+def enumeration(
     non_terminals,
     terminals,
     production_rules,
-    start_symbol,
-    function_declaration,
-    constraint
+    start_symbol
 ):
-    # Updated from verification oracle
-    counterexample_input_set = set()
-
     # Initialize leaf_expressions, non_leaf_production_rules
     leaf_expressions = dict()
     non_leaf_production_rules = dict()
@@ -55,85 +50,116 @@ def bottom_up_tree_search(
         for production_rule_for_non_terminal in production_rules_for_non_terminal:
             if any((
                 component in non_terminals
-                for component in iterate_components_in_expr_in_reverse_polish_notation(production_rule_for_non_terminal)
+                for component in production_rule_for_non_terminal
             )):
                 non_leaf_production_rules_for_non_terminal.add(production_rule_for_non_terminal)
             else:
-                simplified_leaf_expression = simplify(production_rule_for_non_terminal)
+                simplified_leaf_expression = simplify(
+                    build_expr_from_components_in_reverse_polish_notation(
+                        production_rule_for_non_terminal
+                    )
+                )
                 leaf_expressions_for_non_terminal.add(simplified_leaf_expression)
         
         leaf_expressions[non_terminal] = leaf_expressions_for_non_terminal
         non_leaf_production_rules[non_terminal] = non_leaf_production_rules_for_non_terminal
-    
-    non_terminals_to_expressions = leaf_expressions.copy()
+       
+    yield from leaf_expressions[start_symbol]
 
-    non_terminals_to_expression_sizes_to_expressions = RecursiveDefaultDict()
+    # Initialize max_expression_size
     max_expression_size = 1
 
+    # Initialize non_terminals_to_expression_sizes_to_expressions
+    non_terminals_to_expression_sizes_to_expressions = RecursiveDefaultDict()
+    
     for (non_terminal, leaf_expressions_for_non_terminal) in leaf_expressions.items():
         for leaf_expression in leaf_expressions_for_non_terminal:
             non_terminals_to_expression_sizes_to_expressions[non_terminal][max_expression_size][leaf_expression]  
 
+    # Initialize non_terminals_to_expressions
+    non_terminals_to_expressions = leaf_expressions.copy()
+    
     while True:
         # Iterate non-leaf rules of non-terminals
         for non_terminal, non_leaf_production_rules_for_non_terminal in non_leaf_production_rules.items():
             for non_leaf_production_rule in non_leaf_production_rules_for_non_terminal:
-                print('non_terminal', non_terminal, 'non_leaf_production_rule', non_leaf_production_rule, file=stderr)
-
-                components_in_non_leaf_production_rule = list(iterate_components_in_expr_in_reverse_polish_notation(non_leaf_production_rule))
+                # print('non_terminal', non_terminal, 'non_leaf_production_rule', non_leaf_production_rule, file=stderr)
 
                 indices_and_non_terminals_in_non_leaf_production_rule = [
                     (index, component)
-                    for (index, component) in enumerate(components_in_non_leaf_production_rule)
+                    for (index, component) in enumerate(non_leaf_production_rule)
                     if component in non_terminals
                 ]
 
                 # Replace all non-terminals in production rule in every possible way given max_expression_size
-                for sum_of_k_integers in split_n_to_sum_of_k_positive_integers(max_expression_size, len(indices_and_non_terminals_in_non_leaf_production_rule)):
-                    print('\t', 'sum_of_k_integers', sum_of_k_integers, file=stderr)
+                for sum_of_k_integers in split_n_to_sum_of_k_positive_integers(
+                    max_expression_size,
+                    len(indices_and_non_terminals_in_non_leaf_production_rule)
+                ):
+                    # print('\t', 'sum_of_k_integers', sum_of_k_integers, file=stderr)
 
                     for expressions_for_non_terminals_in_non_leaf_production_rule in itertools.product(*[
                         non_terminals_to_expression_sizes_to_expressions[non_terminal_in_non_leaf_production_rule][expression_size]
                         for (index, non_terminal_in_non_leaf_production_rule), expression_size
-                        in zip(indices_and_non_terminals_in_non_leaf_production_rule, sum_of_k_integers)
+                        in zip(indices_and_non_terminals_in_non_leaf_production_rule, sum_of_k_integers)   
                     ]):
-                        print('\t\t', 'expressions_for_non_terminals_in_non_leaf_production_rule', expressions_for_non_terminals_in_non_leaf_production_rule, file=stderr)
+                        # print('\t\t', 'expressions_for_non_terminals_in_non_leaf_production_rule', expressions_for_non_terminals_in_non_leaf_production_rule, file=stderr)
 
-                        components_in_new_non_leaf_production_rule = components_in_non_leaf_production_rule.copy()
+                        new_non_leaf_production_rule = list(non_leaf_production_rule)
 
-                        for (index, non_terminal_in_non_leaf_production_rule), expression_for_non_terminal_in_non_leaf_production_rule in zip(
+                        for (
+                            (index, non_terminal_in_non_leaf_production_rule),
+                            expression_for_non_terminal_in_non_leaf_production_rule
+                        ) in zip(
                             indices_and_non_terminals_in_non_leaf_production_rule,
                             expressions_for_non_terminals_in_non_leaf_production_rule
                         ):
-                            components_in_new_non_leaf_production_rule[index] = expression_for_non_terminal_in_non_leaf_production_rule
+                            new_non_leaf_production_rule[index] = expression_for_non_terminal_in_non_leaf_production_rule
                         
                         simplified_new_expression = simplify(
                             build_expr_from_components_in_reverse_polish_notation(
-                                components_in_new_non_leaf_production_rule
+                                new_non_leaf_production_rule
                             )
                         )
 
                         if simplified_new_expression not in non_terminals_to_expressions[non_terminal]:
                             non_terminals_to_expressions[non_terminal].add(simplified_new_expression)
                             non_terminals_to_expression_sizes_to_expressions[non_terminal][max_expression_size][simplified_new_expression]
-
-        print('non_terminals_to_expression_sizes_to_expressions', non_terminals_to_expression_sizes_to_expressions, file=stderr)
-
-        for candidate_program in non_terminals_to_expression_sizes_to_expressions[start_symbol][max_expression_size]:
-            if all((
-                replace_function_declaration_in_constraint_with_candidate_program(
-                    counterexample_input,
-                    function_declaration,
-                    constraint,
-                    candidate_program
-                ).subs(counterexample_input)
-                for counterexample_input in counterexample_input_set
-            )):
-                counterexample_input = yield candidate_program
-                if counterexample_input is not None:
-                    counterexample_input_set.add(counterexample_input)
-            else:
-                print(f'skipped {candidate_program}')
-                pass
-
+                            if non_terminal == start_symbol:
+                                yield simplified_new_expression
+        
         max_expression_size += 1
+
+    
+def bottom_up_tree_search(
+    non_terminals,
+    terminals,
+    production_rules,
+    start_symbol,
+    function_declaration,
+    constraint
+):
+    # Updated from verification oracle
+    counterexample_input_set = set()
+    
+    for candidate_program in enumeration(
+        non_terminals,
+        terminals,
+        production_rules,
+        start_symbol
+    ):
+        if all((
+            replace_function_declaration_in_constraint_with_candidate_program(
+                counterexample_input,
+                function_declaration,
+                constraint,
+                candidate_program
+            ).subs(counterexample_input)
+            for counterexample_input in counterexample_input_set
+        )):
+            counterexample_input = yield candidate_program
+            if counterexample_input is not None:
+                counterexample_input_set.add(counterexample_input)
+        else:
+            print(f'skipped {candidate_program}')
+            pass
